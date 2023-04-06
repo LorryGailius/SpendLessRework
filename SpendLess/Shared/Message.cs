be50c3e18d450ea3c3428d2bd80b63201f775067
Laurynas <lauris.gailius@gmail.com>
2023-04-06 16:14:58 +0300

    Added ticket and message database objects

---
+++ SpendLess/Shared/Message.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SpendLess.Shared;
+
+public class Message
+{
+    public int Id { get; set; }
+    public int senderID { get; set; }
+    public int ticketID { get; set; }
+    public string? message { get; set; }
+    public DateTime? date { get; set; }
+}
